7c6ab3852456493d83864133eb8ac91106a3793a
jtr109 <lyp_login@outlook.com>
2019-12-31 22:29:43 +0800

    feat(lm): add error_chain

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+#[macro_use]
+extern crate error_chain;
+
 use std::collections::HashMap;
 
+error_chain! {
+    foreign_links {
+        Reqwest(reqwest::Error);
+    }
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     let resp: HashMap<String, String> =
         reqwest::get("https://httpbin.org/ip").await?.json().await?;
     println!("{:#?}", resp);
