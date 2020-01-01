053b1ac57df0d080ef30ca1edbe013a783250a0a
jtr109 <lyp_login@outlook.com>
2020-01-01 11:17:27 +0800

    feat(lm): parse input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::{App, Arg};
 use error_chain::error_chain;
 use serde::{Deserialize, Serialize};
 
@@ -39,15 +40,30 @@ fn convert(body: Body) -> Output {
     Output { items }
 }
 
+fn get_input() -> String {
+    let matches = App::new("Longman Query")
+        .version("0.0.1")
+        .author("Ryan Li <conbas2019@gmail.com>")
+        .about("Query From Longman Dictionary of Contemporary English Online")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Text to query")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+    matches.value_of("INPUT").unwrap().to_string()
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    let q = get_input();
+    let url = format!(
+        "https://www.ldoceonline.com/autocomplete/english/?q={}&contentType=application%2Fjson%3B+charset%3Dutf-8",
+        q,
+    );
     let client = reqwest::Client::new();
-    let body: Body = client
-        .get("https://www.ldoceonline.com/autocomplete/english/?q=test&contentType=application%2Fjson%3B+charset%3Dutf-8")
-        .send()
-        .await?
-        .json()
-        .await?;
+    let body: Body = client.get(&url).send().await?.json().await?;
     let output = convert(body);
     println!("{}", serde_json::to_string(&output)?);
     Ok(())
